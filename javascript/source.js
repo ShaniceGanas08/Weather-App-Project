271def7743e89e9c5f840ddccc6a3ab2863340f4
ShaniceGanas08 <129981331+ShaniceGanas08@users.noreply.github.com>
2023-10-25 23:36:05 +0200

    Changed Icons

--- javascript/source.js
+++ javascript/source.js
@@ -37,8 +37,10 @@ function weatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
-  console.log(response);
   document.querySelector("#date").innerHTML = formatDate(response.data.dt *1000);
+let iconElement = document.querySelector("#icon");
+iconElement.setAttribute = ("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
 }
 
 function search(event) {
